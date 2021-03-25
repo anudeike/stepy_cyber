bf8df5ad1f96734a2b0f6cb34b3bad965dd6a6c5
anudeike <ikechukwuanude@gmail.com>
2021-03-24 22:56:44 -0500

    changed the encoding algorithm

--- main.py
+++ main.py
@@ -4,66 +4,35 @@ import numpy as np
 # import sys
 # np.set_printoptions(threshold=sys.maxsize)
 
-def char_to_binary(c):
-    # helper function to get the correct representation of the binary number we need
-    raw_bin = list(bin(ord(c))[2:])
 
-    # if the length is less than 9 pad it with zeros at the front
-    num_zeros_to_pad = 9 - len(raw_bin)
+def chunks(l, n):
+    n = max(1, n)
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+def convert_to_binary(byte):
+
+    raw = list(bin(byte))[2:]
+
+    # pad the length
+    num_zeros_to_pad = 8 - len(raw)
     padding_zeros = ['0' for _ in range(num_zeros_to_pad)]
 
-    return padding_zeros + raw_bin
+    padded = np.concatenate((padding_zeros, raw))
+    return padded
 
+def convert_to_binary_string(ch):
 
-def convert_message_to_binary(msg):
-    char_list = list(msg)
+    raw = list(bin(ord(ch)))[2:]
 
-    binary_rep_array = []
+    # pad the length
+    num_zeros_to_pad = 8 - len(raw)
+    padding_zeros = ['0' for _ in range(num_zeros_to_pad)]
 
-    for char in char_list:
-        binary_rep_array.append(char_to_binary(char))
+    padded = np.concatenate((padding_zeros, raw))
+    return padded
 
-    return binary_rep_array
+def read_image_and_prepare_data(path="",):
 
-def map_binary_list_to_encoding_region(e, b_c):
-    if int(b_c) == 1:
-        # make sure that it is odd
-        if e % 2 == 0:
-            e += 1
-
-        print("odd")
-    else:
-        # make sure it is even
-        if e % 2 == 1:
-            e -= 1
-        print("even")
-
-    return e
-
-def encode_image(bin_rep, imageMatrix):
-
-    print(f'Image Matrix: {imageMatrix}')
-    print(f'Image Matrix Shape: {imageMatrix.shape}')
-
-    x = 0
-    outputImageMatrix = []
-    for bin_char in bin_rep:
-        # get the encoding region
-        encoding_region = list(chain.from_iterable(imageMatrix[x:x + 3]))
-
-        # map the encoding region to the bin Char array
-        encoding_region_output = list(map(map_binary_list_to_encoding_region, encoding_region, bin_char))
-
-        # append the output
-        outputImageMatrix.append([encoding_region_output[j:j+3] for j in range(0, len(encoding_region_output), 3)])
-
-        # make sure to only get every 3
-        x += 3
-
-
-    return list(chain.from_iterable(outputImageMatrix))
-
-def read_image_and_prepare_data(path=""):
     if path == "":
         raise ValueError("Path cannot be empty.")
 
@@ -71,32 +40,40 @@ def read_image_and_prepare_data(path=""):
     img_dimensions = img.shape[:2]
 
     # flatten
-    img = img.reshape((img_dimensions[0] * img_dimensions[1], 3))
+    img = img.flatten()
 
-    return img, img_dimensions
+    # convert into a bit stream
+    img_bit_stream = np.array(list(map(convert_to_binary, img)))
+
+    return img_bit_stream, img_dimensions
+
+def encode_message_in_img(msg_chunk, img_byte):
+
+    # so take the chunk and replace the talk end of the img byte with it
+    print(f'Chunk from Message: {msg_chunk}')
+    print(f'Byte from Image: {img_byte}')
+
+    new_img_byte = np.concatenate((img_byte[:6], msg_chunk))
+
+    # should return a binary string
+    return "".join(new_img_byte)
 
 def encode(path, msg, output_path = "encode_output.png"):
 
-    # prepare data
-    prepared_img_data, img_dimen = read_image_and_prepare_data(path)
+    # # read the image, prepare by flattening and then getting the bit stream
+    image_bit_stream, image_dimensions = read_image_and_prepare_data(input_image_path)
 
-    # msg
-    msg_binary_representation = convert_message_to_binary(msg)
+    # take the message to be encoded and turn it into a stream of bits.
+    message_bit_stream = np.array(list(map(convert_to_binary_string, list(encoding_msg)))).flatten()
 
-    # encode the image - get the data from the encoder
-    encoded_image_data = np.array(encode_image(msg_binary_representation, prepared_img_data))
+    # divide the message bit stream into chunks of 2
+    message_bit_stream = np.array(chunks(message_bit_stream, 2))
 
-    # assert that the length of the of the encoded image_data is 3 * length of the msg
-    assert encoded_image_data.shape[0] == len(msg) * 3, "The encoded image data should be 3 times the length of the message!"
-
-    # concat the encoded image to the original image
-    output_image_data_flat = np.concatenate((encoded_image_data, prepared_img_data[len(msg) * 3:]), axis=0)
-
-    # reshape the output
-    outputImage = output_image_data_flat.reshape((img_dimen[0], img_dimen[1], 3))
-
-    # write the output
-    cv2.imwrite(output_path, outputImage)
+    # encode the the message stream into the image
+    v = list(map(encode_message_in_img, message_bit_stream, image_bit_stream))
+    #print(message_bit_stream)
+    print(v)
+    pass
 
 def decode(path):
     pass
@@ -104,48 +81,16 @@ def decode(path):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # # # read the image
-    # image = cv2.imread("test.png")
-    # dim = image.shape[:2]
-    #  # (64, 64, 3)
-    #
-    # # flatten the image to make it usable
-    # flattenImage = image.reshape((dim[0] * dim[1], 3))
-    #
-    # msgToBeEncoded = "Hii"
-    #
-    # # convert to binary
-    # binary_rep = convert_message_to_binary(msgToBeEncoded)
-    #
-    # # encode
-    # encoded_image_flat = np.array(encode_image(binary_rep, flattenImage))
-    #
-    # # encoded
-    # print(f"Encoded Image Flat: {encoded_image_flat}")
-    # print(f'Encoded Image Flat Shape: {encoded_image_flat.shape}')
-    #
-    # # concat
-    #
-    # outputImageFlat = np.concatenate((encoded_image_flat, flattenImage[len(msgToBeEncoded) * 3:]), axis=0)
-    #
-    #
-    # print(f"outputImageFlat Image Flat 2: {outputImageFlat}")
-    # print(f'outputImageFlat Flat Shape 2: {outputImageFlat.shape}')
-    #
-    # # output Image
-    # outputImage = outputImageFlat.reshape((dim[0], dim[1], 3))
-    #
-    # print(f'Output: {outputImage.shape}')
-    #
-    # cv2.imwrite('asd.png', outputImage)
+    """
+    How the algorithm works
+    
+    
+    """
+    input_image_path = "test.png"
+    encoding_msg = "Hello!"
+    encode(path=input_image_path, msg=encoding_msg)
 
-    # encode
-
-    # DECODE
-    #img_data, image_dimensions = read_image_and_prepare_data('asd.png')
-
-    #print(img_data)
-
+    exit(0)
 
 
 
